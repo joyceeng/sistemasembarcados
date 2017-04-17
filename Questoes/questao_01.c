c3124c33ca211b7994aa59b7e30e5f0604c093a5
Joyce da Costa <joycindia@hotmail.com>
2017-04-17 09:28:27 -0300

    Delete questao_01.c

--- Questoes/questao_01.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-	int main(int argc, const char * argv[]){
-		write(open ("ola_mundo.txt", O_WRONLY | O_CREAT, S_IRWXU), "Ola mundo!\n", 11);
-		return 0;
-}
